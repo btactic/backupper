d5fcb737a87c179b37abd5222b1ad51bea5baaba
Marc Sánchez <sanchezfauste@gmail.com>
2016-10-07 13:46:42 +0200

    Function get_rsync_error receives exit_value as parameter

--- backupper.sh
+++ backupper.sh
@@ -65,51 +65,52 @@ function send_backup() {
 }
 
 function get_rsync_error() {
-    if [ $rsync_exit_value -eq 0 ]; then
+    local exit_value=$1
+    if [ $exit_value -eq 0 ]; then
         echo "Success"
-    elif [ $rsync_exit_value -eq 1 ]; then
+    elif [ $exit_value -eq 1 ]; then
         echo "Syntax or usage error"
-    elif [ $rsync_exit_value -eq 2 ]; then
+    elif [ $exit_value -eq 2 ]; then
         echo "Protocol incompatibility"
-    elif [ $rsync_exit_value -eq 3 ]; then
+    elif [ $exit_value -eq 3 ]; then
         echo "Errors selecting input/output files, dirs"
-    elif [ $rsync_exit_value -eq 4 ]; then
+    elif [ $exit_value -eq 4 ]; then
         echo "Requested  action not supported: an attempt was made to \
             manipulate 64-bit files on a platform that cannot support them; \
             or an option was specified that supported by the client and not \
             by the server."
-    elif [ $rsync_exit_value -eq 5 ]; then
+    elif [ $exit_value -eq 5 ]; then
         echo "Error starting client-server protocol"
-    elif [ $rsync_exit_value -eq 6 ]; then
+    elif [ $exit_value -eq 6 ]; then
         echo "Daemon unable to append to log-file"
-    elif [ $rsync_exit_value -eq 10 ]; then
+    elif [ $exit_value -eq 10 ]; then
         echo "Error in socket I/O"
-    elif [ $rsync_exit_value -eq 11 ]; then
+    elif [ $exit_value -eq 11 ]; then
         echo "Error in file I/O"
-    elif [ $rsync_exit_value -eq 12 ]; then
+    elif [ $exit_value -eq 12 ]; then
         echo "Error in rsync protocol data stream"
-    elif [ $rsync_exit_value -eq 13 ]; then
+    elif [ $exit_value -eq 13 ]; then
         echo "Errors with program diagnostics"
-    elif [ $rsync_exit_value -eq 14 ]; then
+    elif [ $exit_value -eq 14 ]; then
         echo "Error in IPC code"
-    elif [ $rsync_exit_value -eq 20 ]; then
+    elif [ $exit_value -eq 20 ]; then
         echo "Received SIGUSR1 or SIGINT"
-    elif [ $rsync_exit_value -eq 21 ]; then
+    elif [ $exit_value -eq 21 ]; then
         echo "Some error echoed by waitpid()"
-    elif [ $rsync_exit_value -eq 22 ]; then
+    elif [ $exit_value -eq 22 ]; then
         echo "Error allocating core memory buffers"
-    elif [ $rsync_exit_value -eq 23 ]; then
+    elif [ $exit_value -eq 23 ]; then
         echo "Partial transfer due to error"
-    elif [ $rsync_exit_value -eq 24 ]; then
+    elif [ $exit_value -eq 24 ]; then
         echo "Partial transfer due to vanished source files"
-    elif [ $rsync_exit_value -eq 25 ]; then
+    elif [ $exit_value -eq 25 ]; then
         echo "The --max-delete limit stopped deletions"
-    elif [ $rsync_exit_value -eq 30 ]; then
+    elif [ $exit_value -eq 30 ]; then
         echo "Timeout in data send/receive"
-    elif [ $rsync_exit_value -eq 35 ]; then
+    elif [ $exit_value -eq 35 ]; then
         echo "Timeout waiting for daemon connection"
     else
-        echo "Returned exit value: $rsync_exit_value"
+        echo "Returned exit value: $exit_value"
     fi
 }
 
@@ -190,7 +191,7 @@ if [ ${#DIRS_TO_BACKUP[@]} -eq 0 ]; then
 else
     send_backup
     if [ $rsync_exit_value -ne 0 ]; then
-        rsync_error_message=$(get_rsync_error)
+        rsync_error_message=$(get_rsync_error $rsync_exit_value)
         echo -e "Error executing rsync ($rsync_exit_value): $rsync_error_message"
     fi
 fi
