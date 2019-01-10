50d263820a3d315d3653af7168cb09ea689ef82c
Adrian Gibanel bTactic <adrian.gibanel@btactic.com>
2019-01-10 18:10:44 +0100

    Added proper copyrights to the scripts

--- install.sh
+++ install.sh
@@ -1,5 +1,27 @@
 #!/bin/bash
 
+#######################################################################
+# backupper: Incremental backups with rsync and hard links.
+
+# Copyright (C) 2017,2018,2019 BTACTIC, SCCL
+# Copyright (C) 2019 Adrián Gibanel López
+# Copyright (C) 2017,2018 Marc Sanchez Fauste
+# Copyright (C) 2017,2018 Adria Sanchez Mas
+
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#######################################################################
+
 CONFIG_DIR="/usr/local/etc/backupper"
 BINARY_DIR="/usr/local/bin"
 BINARY_FILE="backupper.sh"
